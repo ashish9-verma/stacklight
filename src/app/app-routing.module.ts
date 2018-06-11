e849ce70c267f85df318d0d1930f721339608d1e
Ashish <akverma.ashish@gmail.com>
2018-06-11 12:23:05 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AlertComponent } from './alert/alert.component';
+import { HistoryComponent } from './history/history.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+{
+	path : '',
+	component : DashboardComponent
+},
+{
+	path : 'dashboard',
+	component : DashboardComponent
+},
+{
+	path : 'alert',
+	component : AlertComponent
+},
+{
+	path : 'history',
+	component : HistoryComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AlertComponent } from './alert/alert.component';
+import { HistoryComponent } from './history/history.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AlertComponent,
+    HistoryComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
